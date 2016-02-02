6946caf65ea4caf744fa383cdac01f186ad2f85b
lmills1693 <millsl@mail.gvsu.edu>
2016-02-02 12:51:18 -0500

    Update Wallace.java

--- Wallace.java
+++ Wallace.java
@@ -14,6 +14,7 @@ public class Wallace extends Robot
 {
 
 	public double halfWidth,halfHeight;
+	public int gotHit,hit,corner;
 
 	/**
 	 * run: Wallace's default behavior
@@ -33,20 +34,23 @@ public class Wallace extends Robot
 		// Robot main loop
 		while(true) {
 	
-			ahead(30);
+			
 			if(!inCorner())
 				goCorner();
-		
+			scan();
 		
 			
-		}
-	}
+	}}
 
 	/**
 	 * onScannedRobot: What to do when you see another robot
 	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
 		// Replace the next line with any behavior you would like
+		
+		if(e.getDistance() < 80 && e.getEnergy() < 15){
+		return;
+		}
 		fire(5);
 		
 	}
@@ -78,8 +82,11 @@ public class Wallace extends Robot
 		}
 	
 		if (getX()> halfWidth && getY() > halfHeight){
-			if (getX() <= getBattleFieldWidth() - padding && getY() <= getBattleFieldHeight()-padding){
-				System.out.println("Top right ");
+			if (getX() < getBattleFieldWidth() - padding && getY() < getBattleFieldHeight()-padding){
+				System.out.println("Top right " + getX() + " " + getY());
+				if(getRadarHeading() != 90){
+				System.out.println();
+				}
 				return false;
 			}
 		}
@@ -91,55 +98,96 @@ public class Wallace extends Robot
 				return false;
 			}
 		}
-		System.out.println("We are in a corner");
+		//System.out.println("We are in a corner");
 		return true;
 	}
 
 	public void onHitByBullet(HitByBulletEvent e) {	
 		 
-	if(inCorner())
-		back(70);	
+		
+		
+		gotHit ++;
+		if(e.getHeading()% 90 > 5){
+			return;
+		}
+		double dodgeHeading  = e.getHeading() + 90;
+		dodgeHeading  = dodgeHeading % 360; 
+		turnDodge(dodgeHeading);
+		System.out.println("heading of bullet = " + e.getHeading());
+		System.out.println("dodging" + dodgeHeading);
+		
+		
+		
+		
 		
 
-		
-		System.out.println(getX() + "  " + getY());
 
-		
-		//if (e.getBearing() > -95 && e.getBearing() < 0 ){
-		//ahead(200);
-		//}
+
      
 	 
 }
-	public void goCorner(){
-		getCorner();
-		ahead(5000);
-		turnLeft(90);
-		ahead(5000);
-		turnGunLeft(90);
-		turnRadarLeft(0);
+	public void turnDodge( double heading) {
+		
+		if(heading - getHeading() < 5){
+			ahead(75);
+			back(75);
+		}
+		if((heading - (getHeading() +180) % 360) < 5){
+			
+			back(75);
+			ahead(75);
+		}
+	/*	if (heading - getHeading() < 180 && heading - getHeading() > 0){
+			System.out.println("right");
+			double turn =  heading - getHeading();
+			turnRight(turn);
+		}else{
+						System.out.println("left");
+		
+			double turn = (360 + getHeading()) - heading;
+			turn = turn % 360;
+			turnLeft(turn);
+		}*/
 
 	}
-	public void getCorner() {
+	public void goCorner(){
 		
+		
+
+	double gunH = 0;
+		System.out.println("going corner");
 		//Top left
 		if (getX()< halfWidth && getY() > halfHeight){
 			turnRight(normalRelativeAngleDegrees(0 - getHeading()));
+			//System.out.println("top left");
+			gunH = 270;
+			corner = 1;
 			}
 		
 		//Bottom left
 		if (getX() < halfWidth && getY()<halfHeight){
 			turnRight(normalRelativeAngleDegrees(270 - getHeading()));
+			gunH = 180;
 		}
 		
 		//Top right 
-		if (getX() > halfWidth && getY()>halfHeight){
+		if (getX() >= halfWidth && getY()>=halfHeight){
 			turnRight(normalRelativeAngleDegrees(90 - getHeading()));
+			gunH = 0;
 		}
 		
 		//Bottom right
 		if (getX() > halfWidth && getY()<halfHeight){
+			turnRight(normalRelativeAngleDegrees(180 - getHeading()));
+			gunH = 90 ; 
 		}
+		
+		turnGunRight(normalRelativeAngleDegrees(gunH - getGunHeading()));
+		ahead(5000);
+		turnLeft(90);
+		ahead(5000);
+		//turnGunLeft(90);
+		//turnRadarLeft(0);
 	}
 	
 	public void onHitRobot(HitRobotEvent e) {
@@ -151,7 +199,7 @@ public class Wallace extends Robot
 	
 	public void onBulletHit(HitByBulletEvent e){
 		
-
+	
 	
 	}
 	/**
