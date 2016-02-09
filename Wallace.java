6dde794771807109edb7bcff226eba0e6e9aa087
Lauren <millsl@mail.gvsu.edu>
2016-02-09 00:39:08 -0500

    documented code

--- Wallace.java
+++ Wallace.java
@@ -1,24 +1,18 @@
 import robocode.*;
-
 import java.awt.Color;
 import static robocode.util.Utils.normalRelativeAngleDegrees;
 
-
-
-// API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
-
-/**
- * Wallace - a robot by (your name here)
- */
+/****************************************************************************
+ * Wallace - a robot by Lauren Mills,, Kyle Niewiada, and Alex Stuart
+ ****************************************************************************/
 public class Wallace extends Robot
 {
-
 	public double halfWidth,halfHeight;
-	public int gotHit,hit,corner;
+	public int hit,corner;
 	public boolean firstScan = false;
-	/**
-	 * run: Wallace's default behavior
-	 */
+	/****************************************************************************
+	 * Runs Wallace's default behavior 
+ 	****************************************************************************/
 	public void run() {
 		//get Height and width of battle field
 		halfWidth = .5 * getBattleFieldWidth();
@@ -31,12 +25,9 @@ public class Wallace extends Robot
 		setBulletColor(Color.red);
 		setScanColor(Color.red);
 		
-
-
 		// 360 scan to find walls bearing and move opposite direction(inside scanned robot)
 		turnRadarRight(360);
  	
- 		//set high level variable to staore wlls initial heading 
 		goCorner(); // in the opposite heading 
 		
 		// Robot main loop
@@ -45,112 +36,116 @@ public class Wallace extends Robot
 				goCorner();
 			}
 		scan(); // make a radar sweeping method to scan that 90* 
-		
-		
-		
-		
+			
 		}
 	}
 
-	/**
-	 * onScannedRobot: What to do when you see another robot
-	 */
+	/****************************************************************************
+	 * When Wallace scans an opponent he shoots if appropriate
+ 	****************************************************************************/
 	public void onScannedRobot(ScannedRobotEvent e) {
-		// Replace the next line with any behavior you would like
+		
+		//after the inital scan determine what quadurant we have spawned in and 
+		//head to the closest wall
 		if(!firstScan){
-
+		
 			int quad = (int) e.getHeading() /90;
-			//System.out.println(quad);
 			double opponentHeading = quad * 90.0;
-			//System.out.println("opponent Bearing : " + opponentHeading);
-			
-			turn((opponentHeading + 180)% 360);//probably wrong
+			turn((opponentHeading + 180)% 360);
 			ahead(400);//move ahead until hitting a wall,then go to corner
 		}
-		//TODO: determine best value later
+		
+		//If walls is close and has low energy and avoid firing to avoid 
+		//walls going into his disabled path
 		if(e.getDistance() < 150 && e.getEnergy() < 20){
 		System.out.println("not firing");
 		return;
 		}
+		
+		//fire 5 if walls is scanned
 		if(firstScan)
 		fire(5);
 		firstScan = true;
 	
 	}
 
-	/**
-	 * onHitByBullet: What to do when you're hit by a bullet
-	 */
-	
-// ADD EQUALLLLLLL
+	/****************************************************************************
+	 * The inCorner method determines if Wallace has made it to a corner
+	 * and returns a corresponding boolean value
+ 	****************************************************************************/
 	public boolean inCorner(){
+		
+		//sets padding to the size of Wallace
 		int padding = 21;
-		
-		
+	
+        //Determines wallace needs to be in the top left corner but is not
 		if (getX()< halfWidth && getY() > halfHeight){
 			if (getX() > padding && getY() < getBattleFieldHeight()-padding){
-				//System.out.println("Top Left");
-				return false;
-				
+				return false;		
 			}
 		}
         
-		
+		//Determines wallace needs to be in the lower left corner but is not
 		if (getX()< halfWidth && getY() < halfHeight){
 			if (getX() > padding && getY() > padding){
-				//System.out.println("Lower Left");
 				return false;
 				
 			}
 		}
 	
+		//Determines wallace needs to be in the top right corner but is not
 		if (getX()> halfWidth && getY() > halfHeight){
 			if (getX() < getBattleFieldWidth() - padding && getY() < getBattleFieldHeight()-padding){
-				//System.out.println("Top right " + getX() + " " + getY());
-				if(getRadarHeading() != 90){
-				//System.out.println();
-				}
 				return false;
 			}
 		}
 		
-		
+		//Determines wallace needs to be in the lower right corner but is not
 		if (getX()>= halfWidth && getY() <= halfHeight){
 			if (getX() < getBattleFieldWidth() - padding && getY() > padding){
-				//System.out.println("lower right");
 				return false;
 			}
 		}
-		//System.out.println("We are in a corner");
+		
+		//Wallace is in a corner
 		return true;
 	}
-	//TODO: check later
+
+	/****************************************************************************
+	 * When hit by a bullet dodge
+ 	****************************************************************************/
 	public void onHitByBullet(HitByBulletEvent e) {	
-		gotHit ++;
+		
 		if(e.getHeading()% 90 > 5){
 			return;
 		}
+		//Determine the robots heading to determine what way to dodge
 		double dodgeHeading  = e.getHeading() + 90;
 		dodgeHeading  = dodgeHeading % 360; 
 		turnDodge(dodgeHeading);
-		//System.out.println("heading of bullet = " + e.getHeading());
-		//System.out.println("dodging" + dodgeHeading);
 	}
 	
+	/****************************************************************************
+	 * This method determines which direction we dodge based on dodgeHeading
+ 	****************************************************************************/
 	public void turnDodge( double heading) {
 		
 		if(heading - getHeading() < 5){
 			ahead(75);
 			back(75);
 		}
+		
 		if((heading - (getHeading() +180) % 360) < 5){
 			
 			back(75);
 			ahead(75);
 		}
-	
-
 	}
+	
+	/****************************************************************************
+	 * This method determines which direction to turn at the begining of the
+	 * battle
+ 	****************************************************************************/
 	public void turn (double heading){
 		if (heading - getHeading() < 180 && heading - getHeading() > 0){
 			//System.out.println("right");
@@ -165,16 +160,19 @@ public class Wallace extends Robot
 		}
 	}
 	
+	/****************************************************************************
+	 * This method determines which corner to go to and moves wallace to the
+	 * appropriate corner
+ 	****************************************************************************/
 	public void goCorner(){
 		
 		
 
 	double gunH = 0;
-		//System.out.println("going corner");
+	
 		//Top left
 		if (getX()< halfWidth && getY() > halfHeight){
 			turnRight(normalRelativeAngleDegrees(0 - getHeading()));
-			////System.out.println("top left");
 			gunH = 270;
 			corner = 1;
 			}
@@ -197,33 +195,25 @@ public class Wallace extends Robot
 			gunH = 90 ; 
 		}
 		
+		//move to corner
 		turnGunRight(normalRelativeAngleDegrees(gunH - getGunHeading()));
 		ahead(5000);
 		turnLeft(90);
 		ahead(5000);
-		//turnGunLeft(90);
-		//turnRadarLeft(0);
 	}
 	
+	/****************************************************************************
+	 * If wallace hits a robot fire
+ 	****************************************************************************/
 	public void onHitRobot(HitRobotEvent e) {
-	
-		//back(500);
-		getEnergy();
 		fire(3);
 	}
 	
-	public void onBulletHit(HitByBulletEvent e){
-		
-	
-	
-	}
-	/**
-	 * onHitWall: What to do when you hit a wall
-	 */
+	/****************************************************************************
+	 * If wallace hits a wall move back 3
+ 	****************************************************************************/
 	public void onHitWall(HitWallEvent e) {
-		// Replace the next line with any behavior you would like
 		back(3);
 	}	
-	
 	
 }
